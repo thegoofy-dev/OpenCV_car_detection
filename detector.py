e73bca48fa3219c7853dcc5c369df711b0eeb3f2
Pankaj Tyagi <itstyagi0001@gmail.com>
2024-03-19 21:52:19 +0530

    passed the classifier

--- detector.py
+++ detector.py
@@ -19,6 +19,8 @@ def rescaleFrame(frame, scale=2):
     # returning the resized frame
     return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
 
+car_classifier = cv2.CascadeClassifier('cars.xml')
+
 while True:
     isTrue, frame = capture.read()
 
@@ -30,6 +32,17 @@ while True:
     # Resized Frame
     cv2.imshow("Resized Media Player", frame_resized)
     
+    # Converting the Resized_frame color BGR to Grayscale 
+    gray = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2GRAY)
+
+    # Passing our frames to our car classifier!
+    cars = car_classifier.detectMultiScale(gray, 1.4, 2)
+
+    # Extract bounding bpxes for any bodies identified
+    for (x,y,w,h) in cars:
+        cv2.rectangle(frame_resized, (x,y),(x+w, y+h), (0,0,255), 2)
+        cv2.imshow("Car Detector", frame_resized)
+
     # Breakpoint of the VideoScreen (By pressing 'x' or 'X' video will close)
     if cv2.waitKey(20) & 0xFF== ord('x'):
         break
